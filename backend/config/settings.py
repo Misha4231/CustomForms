af9490e033b8fb5d4814fdfc02f84d9499276dbc
Misha4231 <mishagalko1@gmail.com>
2025-05-07 14:08:30 +0200

    fix configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "allauth.account.middleware.AccountMiddleware", # auth
+    #"allauth.account.middleware.AccountMiddleware", # auth
 ]
 
 ROOT_URLCONF = 'config.urls'
